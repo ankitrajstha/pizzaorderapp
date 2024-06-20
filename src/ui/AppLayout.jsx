8de71be64ecd390385c9bb97a6f3f2f220191c49
Ankit Shrestha <ankitrajstha>
2024-06-20 13:18:56 +0545

    feat: add children routes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
       <Header />
       <main>
-        <h1>content</h1>
+        <Outlet />
       </main>
       <CartOverview />
     </div>
