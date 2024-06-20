7cc24057d0da198c2e1e2f941f57d95642ae3ed5
Ankit Shrestha <ankitrajstha>
2024-06-20 14:16:55 +0545

    feat: add search order

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
-        errorElement: <Error/>
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -36,6 +36,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
