9c592871eff2be09e7b023ad765d0bd06cfdb74d
Ankit Shrestha <ankitrajstha>
2024-06-21 21:52:57 +0545

    feat: update cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,9 +1,9 @@
-import LinkButton from "../../ui/LinkButton";
-import Button from "../../ui/Button";
-import CartItem from "./CartItem";
-import { useDispatch, useSelector } from "react-redux";
-import { clearCart, getCart } from "./cartSlice";
-import EmptyCart from "./EmptyCart";
+import LinkButton from '../../ui/LinkButton';
+import Button from '../../ui/Button';
+import CartItem from './CartItem';
+import EmptyCart from './EmptyCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
@@ -13,14 +13,14 @@ function Cart() {
   if (!cart.length) return <EmptyCart />;
 
   return (
-    <div className="py-3 px-4">
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt07 text-xl font-semibold">Your cart, {username}</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
-      <ul className="divide-y divide-stone-200 border-b mt-3">
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
@@ -28,6 +28,7 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
+
         <Button type="secondary" onClick={() => dispatch(clearCart())}>
           Clear cart
         </Button>
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,9 +1,10 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
+
   return (
     <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
@@ -12,8 +13,10 @@ function Menu() {
     </ul>
   );
 }
-export const loader = async () => {
+
+export async function loader() {
   const menu = await getMenu();
   return menu;
-};
+}
+
 export default Menu;
